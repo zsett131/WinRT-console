f66ade08304a655e3c75ca7dd7cf41d142d27345
zdsetterquist9926 <zdsetterquist9926@HE203PC18>
2021-09-30 15:01:40 -0400

    Add project files.

---
+++ WinRT-console/WinRT-console.cpp
@@ -0,0 +1,24 @@
+// Zachary Setterquist
+// COP 3003
+// WinRT-console.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// https://docs.microsoft.com/en-us/windows/uwp/cpp-and-winrt-apis/get-started
+
+#include "pch.h"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::Web::Syndication;
+
+int main()
+{
+    winrt::init_apartment();
+
+    Uri rssFeedUri{ L"https://blogs.windows.com/feed" };
+    SyndicationClient syndicationClient;
+    SyndicationFeed syndicationFeed = syndicationClient.RetrieveFeedAsync(rssFeedUri).get();
+    for (const SyndicationItem syndicationItem : syndicationFeed.Items())
+    {
+        winrt::hstring titleAsHstring = syndicationItem.Title().Text();
+        std::wcout << titleAsHstring.c_str() << std::endl;
+    }
+}
